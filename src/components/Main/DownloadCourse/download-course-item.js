148ecbcccafc33ad06a1886c0653d0f1dbb53dea
Hieu Duong <duonghieu121097@gmail.com>
2020-08-22 22:36:00 +0700

    Add download-course

---
+++ src/components/Main/DownloadCourse/download-course-item.js
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect } from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import ThemedText from '../../Common/themed-text'
+import ScreenContainer from '../../Common/screen-container'
+import { CoursesContext } from '../../../provider/courses-provider'
+import { Rating } from 'react-native-elements'
+import { CourseDetailContext } from '../../../provider/courseDetail-provider'
+import { FavouritesContext } from '../../../provider/favourites-provider'
+
+const FavoriteCourseItem = (props) => {
+    const favoriteContext = useContext(FavouritesContext)
+    const coursesContext = useContext(CoursesContext)
+    const courseDetailContext = useContext(CourseDetailContext)
+    
+    const Image_Http_URL = { uri: props.item.courseImage }
+
+    // useEffect(() => {
+    //     coursesContext.getCourseInformation(props.item.id)
+    // }, [])
+
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                // coursesContext.getCourseInformation(props.item.id)
+                favoriteContext.getCourseLikeStatus(props.item.id)
+                courseDetailContext.getCourseDetailWithLession(props.item.id)
+                props.navigation.navigate("CourseDescriptions", {item: props.item})
+            }}
+        >
+            <ScreenContainer style={styles.item}>
+                <Image source={Image_Http_URL} style={styles.image} />
+                <View style={{ margin: 5}}>
+                    <ThemedText>{props.item.courseTitle}</ThemedText>
+                    <ThemedText>{props.item.instructorName}</ThemedText>
+                    <ThemedText style={styles.darkText}>{props.item.coursePrice} VND </ThemedText>
+                    <Rating imageSize={20} fractions="{1}" startingValue={props.item.courseContentPoint} />
+                </View>
+            </ScreenContainer>
+        </TouchableOpacity>
+    )
+}
+
+export default FavoriteCourseItem
+
+const styles = StyleSheet.create({
+    item: {
+        margin: 6,
+        height: 121,
+        width: 400,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        borderBottomColor: 'gray',
+        borderBottomWidth: 1
+
+    },
+    image: {
+        width: 120,
+        height: 120
+    },
+    darkText: {
+        color: 'gray'
+    }
+})
---
+++ src/components/Main/DownloadCourse/download-course.js
@@ -0,0 +1,51 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import ThemedText from '../../Common/themed-text';
+import ScreenContainer from '../../Common/screen-container';
+import { FavouritesContext } from '../../../provider/favourites-provider';
+import { FlatList } from 'react-native-gesture-handler';
+import FavoriteCourseItem from './favorite-course-item';
+import { useFocusEffect } from '@react-navigation/native';
+
+const DownloadCourse = (props) => {
+    const favoriteContext = useContext(FavouritesContext)
+    const [courses, setCourses] = useState([])
+    
+    useEffect(() => {
+            // favoriteContext.getFavoriteCourses()
+            setCourses(favoriteContext.state.data)
+        }, [favoriteContext.state.data])
+
+    if (courses.length === 0) {
+        return (
+            <ScreenContainer style={styles.iconItem}>
+                {favoriteContext.state.isLoading && <ActivityIndicator size="small" color="gray" />}
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <Icon
+                        name='download' size={60} color='darkgray' />
+                </View>
+                <ThemedText style={{ textAlign: 'center', color: 'darkgray' }}>Courses you download will appear here</ThemedText>
+            </ScreenContainer>
+        )
+    } else {
+        return (
+            <ScreenContainer>
+                <FlatList
+                    data={courses}
+                    renderItem={({ item }) => <FavoriteCourseItem navigation={props.navigation} item={item} />}
+                />
+            </ScreenContainer>
+        )
+    }
+}
+
+export default DownloadCourse
+
+const styles = StyleSheet.create({
+    iconItem: {
+        flex: 1,
+        paddingHorizontal: 10,
+        justifyContent: 'center'
+    }
+})
